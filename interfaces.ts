d6981ee114c2e052addcf5894445086c2ad1fd15
Daniel-Stafford <stafford.daniel@gmail.com>
2019-11-29 09:37:51 +0200

    refactor printSummaray interface

--- interfaces.ts
+++ interfaces.ts
@@ -1,19 +1,28 @@
-interface Vehicle {
-	name: string
-	year: number
-	broken: boolean
+interface Reportable {
+	summary(): string
 }
 
 const oldCivic = {
 	name: 'civic',
-	year: 2000,
-	broken: true
+	year: new Date(),
+	broken: true,
+	summary(): string {
+		return `Name: ${this.name}`
+	}
 }
 
-const printVehicle = (vehicle: Vehicle): void => {
-	console.log(`Name: ${vehicle.name}`)
-	console.log(`Year: ${vehicle.year}`)
-	console.log(`Broken: ${vehicle.broken}`)
+const myDrink = {
+	color: 'brown',
+	carbonated: true,
+	sugar: 50,
+	summary(): string {
+		return `My string has ${this.sugar} grams of sugar`
+	}
 }
 
-printVehicle(oldCivic)
+const printSummary = (item: Reportable): void => {
+	console.log(item.summary())
+}
+
+printSummary(oldCivic)
+printSummary(myDrink)
